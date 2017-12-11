55b3e315d5df3725fff217b044e8a6b9605df1cf
Monchai <monchai.j@seven.co.th>
2017-12-11 22:35:28 +0700

    lern new spread operator [...array] and filter destructuring

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,9 +1,46 @@
 import { createStore,combineReducers } from 'redux';
+import uuid from 'uuid';
+
+// Spread Operator [...Array] like concat
+// 1. return new array
+// 2. can control index position in new array ['new',...Array,'new2'] = ['naw',Array,'new2']
+
+const addExpense = (
+        {
+            description = '',
+            note='',
+            amount=0,
+            createAt=0
+        } ={ }
+    ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id:uuid(),
+        description,
+        note,
+        amount,
+        createAt
+    }
+});
+
+const removeExpense = ({id}={})=> (
+    {
+        type: 'REMOVE_EXPENSE',
+        id:id
+    }
+);
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState,action)=>{
     switch(action.type){
+        case "ADD_EXPENSE":
+            return [
+                ...state,
+                action.expense
+            ];
+        case "REMOVE_EXPENSE":
+            return state.filter(({id})=>id !== action.id);
         default:
             return state;
     }
@@ -30,7 +67,16 @@ const store = createStore(
     })
 );
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({description:"Rent", amount:100}));
+const expenseTwo = store.dispatch(addExpense({description:"Coffee", amount:300}));
+
+store.dispatch(removeExpense({id:expenseOne.expense.id}));
+
+console.log(expenseOne);
 
 const demoState = {
     expenses:[{
